b2c5a61196137e45ff3e7c5dba1cf2181303226a
GagenvirGill <gagenvirg@gmail.com>
2025-04-11 15:18:06 -0700

    Removed the adding and removing items to/from outfits controllers and routes as those are no longer needed in the template system

--- src/routes/outfitRoutes.js
+++ src/routes/outfitRoutes.js
@@ -4,8 +4,6 @@ import {
 	getAllOutfits,
 	createOutfit,
 	deleteOutfit,
-	addItemsToOutfit,
-	removeItemsFromOutfit,
 } from "../controllers/outfitController.js";
 
 const router = Router();
@@ -13,8 +11,5 @@ const router = Router();
 router.get("/", getAllOutfits);
 router.post("/", createOutfit);
 router.delete("/:outfitId", deleteOutfit);
-
-router.post("/:outfitId/items", addItemsToOutfit);
-router.delete("/:outfitId/items", removeItemsFromOutfit);
 
 export default router;
