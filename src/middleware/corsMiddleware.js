124fb3c5edb44c325b4b638b505180c960ba7bc4
GagenvirGill <gagenvirg@gmail.com>
2025-05-01 08:39:55 -0700

    Updated the cors middle frontend url to use the env

--- src/middleware/corsMiddleware.js
+++ src/middleware/corsMiddleware.js
@@ -1,8 +1,9 @@
 // src/middleware/corsMiddleware.js
 import cors from "cors";
+import envConfig from "../config/envConfig";
 
 const corsMiddleware = cors({
-	origin: "https://yourfashionfits.vercel.app",
+	origin: `https://${frontendUrl}`,
 	credentials: true,
 	allowedHeaders: ["Content-Type", "Authorization"],
 });
